d4cb32d4c886606f48eac642075a13b07854c1c7
norahev <norahev@users.noreply.github.com>
2020-02-24 13:20:54 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -6,18 +6,24 @@ int main(void)
     int n;
     do
     {
-        n = get_int ("Height: ");
+        n = get_int("Height: "); //prompt user for number
     }
-    while (n < 1 || n > 8);
-    for (int i = 0; i < n; i++)
+    while (n < 1 || n > 8); //repeat until number fits the parameters
+    for (int i = 0; i < n; i++) //determines and repeats line #
     {
-        for (int k = i; k < n - 1; k++)
-        printf(" ");
+        for (int k = i; k < n - 1; k++) //determines and repeats " "
+        { 
+            printf(" ");
+        }
+
         {
             for (int j = 0; j <= i; j++)
-            printf("#");
+             {
+                 printf("#");
+             }
             printf("\n");
-        }
+
+        }//end
     }
 
 
