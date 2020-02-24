141aac422736eea82e801eebca6cdc6e3e2af506
norahev <norahev@users.noreply.github.com>
2020-02-24 16:20:08 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -8,28 +8,28 @@ int main(void)
     {
         h = get_int("Height: "); //prompt user for number
     }
-    while(h < 1 || h > 8); //repeat until number fits parameters
-    for(int l = 0; l < h; l++)
+    while (h < 1 || h > 8); //repeat until number fits parameters
+    for (int l = 0; l < h; l++)
     {
-        for(int d = l; d < h - 1; d++)
+        for (int d = l; d < h - 1; d++) //spaces on left 
         {
             printf(" ");
         }
         {
-            for(int f = 0; f <= l; f++)
+            for (int f = 0; f <= l; f++) //left side of pyramid
             {
                 printf("#");
             }
         }
-        for(int g = 0; g < 1; g++)
+        for (int g = 0; g < 1; g++) //spaces inbetween pyramids
         {
             printf("  ");
         }
-        for(int n = 0; n <= l; n++)
-            {
-                printf("#");
-            }
+        for (int n = 0; n <= l; n++) //right pyramid
+        {
+            printf("#");
+        }
         printf("\n");
     }
 
-}
+} //end
