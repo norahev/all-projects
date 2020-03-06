d2f77b4419e1889ba9bc46d025869bfe7cb5b2e9
norahev <norahev@users.noreply.github.com>
2020-03-06 12:53:52 +0000

    automated commit by check50 [check50=True]

--- plurality.c
+++ plurality.c
@@ -68,7 +68,7 @@ bool vote(string name)
 {
     for (int i = 0; i < MAX; i++)
     {
-        if (!strcmp(candidates[i].name, name))
+        if (strcmp(candidates[i].name, name) == true)
         {
             candidates[i].votes++;
             return true;
