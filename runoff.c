a2a64b8beb4feece1ecf2e5220937a2f440f3759
norahev <norahev@users.noreply.github.com>
2020-03-09 15:44:05 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -172,9 +172,13 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    int min = 0;
+    int min = -1;
     for (int i = 0; i < candidate_count; i++)
     {
+        if (min < 0)
+        {
+            min = candidates[i].votes;
+        }
         if (min > candidates[i].votes && !candidates[i].eliminated)
         {
             min = candidates[i].votes;
