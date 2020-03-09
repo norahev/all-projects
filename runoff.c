31f9b9337b9b967dd240f8714ee2f53b4cf8fefe
norahev <norahev@users.noreply.github.com>
2020-03-09 16:11:27 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -146,9 +146,10 @@ void tabulate(void)
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            if (candidates[j].eliminated == false && preferences[i][j] == 0)
+            if (candidates[preferences[i][j]].eliminated == false)
             {
                 candidates[preferences[i][j]].votes ++; 
+                break;
             }
         }
     }
