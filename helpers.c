912598c751196c9c9e9f1e59e6c602b858e5a1bd
norahev <norahev@users.noreply.github.com>
2020-03-13 14:25:05 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -21,6 +21,30 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    float sepiaR = 0;
+    float sepiaG = 0;
+    float sepiaB = 0;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            sepiaR = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
+            sepiaG = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue;
+            sepiaB = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue;
+            if (sepiaR, sepiaG, sepiaB <=255.0 && sepiaR, sepiaG, sepiaB >= 0.0)
+            {
+                image[i][j].rgbtRed = round(sepaiR);
+                image[i][j].rgbtGreen = round(sepiaG);
+                image[i][j].rgbtBlue = round(sepiaB);
+            }
+            if (sepiaR, sepiaG, sepiaB > 255.0)
+            {
+                image[i][j].rgbtRed = 255;
+                image[i][j].rgbtGreen = 255;
+                image[i][j].rgbtBlue = 255;
+            }
+        }
+    }
     return;
 }
 
