7ba315de59bb29f5eb81cbd3f5bf00bd504fcbb3
norahev <norahev@users.noreply.github.com>
2020-03-13 12:23:13 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -150,12 +150,12 @@ void add_pairs(void)
 
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
-{  
+{
     bool swap = false;
     int t;
     for (int i = 0; i < pair_count; i++)
     {
-        for (int j = i  + 1; j < pair_count; j++)
+        for (int j = 1; j < pair_count; j++)
         {
             if (preferences[pairs[i].winner][pairs[i].loser] < preferences[pairs[j].winner][pairs[j].loser])
             {
