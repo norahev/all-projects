d5f7c11379a1b55776e5410602407745b09c1d8d
norahev <norahev@users.noreply.github.com>
2020-03-13 13:56:48 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -4,15 +4,15 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    int avr = 0;
-    for (int i = 0; i <= height; i++)
+    float avr = 0;
+    for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j <= width; j++)
+        for (int j = 0; j < width; j++)
         {
-            avr = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
-            image[i][j].rgbtBlue = avr;
-            image[i][j].rgbtGreen = avr;
-            image[i][j].rgbtRed = avr;
+            avr = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0;
+            image[i][j].rgbtBlue = round(avr);
+            image[i][j].rgbtGreen = round(avr);
+            image[i][j].rgbtRed = round(avr);
         }
     }
     return;
