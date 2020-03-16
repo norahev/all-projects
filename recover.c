7e78770ed77cc4a1b1db3c638280a1e0ca2f2859
norahev <norahev@users.noreply.github.com>
2020-03-16 16:35:02 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -1,64 +1,163 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
+#include <stdint.h>
+
 #include <cs50.h>
 
+#define BLOCK_SIZE 512
+
+
+
+typedef uint8_t BYTE;
+
+
+
 int main(int argc, char *argv[])
+
 {
-    char *image = argv[1];
+
+
+
+    // remember filenames and open the input file
+
+    char* infile = argv[1];
+
+    FILE* inptr = fopen(argv[1], "r");
+
+// ensure proper usage
+
     if (argc != 2)
+
     {
-        fprintf(stderr, "Usage: ./recover image\n ");
+
+        printf("Only the raw file name");
+
         return 1;
+
     }
-    FILE *inptr = fopen(image, "r");
+
+
+
     if (inptr == NULL)
+
     {
-        fprintf(stderr, "Could not open %s.\n", image);
+
+        printf("Could not open %s.\n", infile);
+
         return 2;
-    }
-    unsigned char buffer[512];
-    int openfile = 0;
-    int jpeg = 0;
-    char filename[10];
-    FILE *img_file = NULL;
-    while (fread(&buffer, 512, 1, inptr))
-    {
-        if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
-        {
-            if (openfile)
-            {
-                fclose(img_file);
-                openfile = 0;
-                sprintf(filename, "%03d.jpg", jpeg);
-                openfile = 1;
-                jpeg ++;
-            }
-            if (!openfile)
-            {
-                sprintf(filename, "%03d.jpg", jpeg);
-                img_file = fopen(filename, "w");
-                openfile = 1;
-                jpeg++;
-            }
-            fwrite(&buffer, 512, 1, img_file);
-            return 0;
-        }
-        else
-        {
-            if(openfile)
-            {
-                fwrite(&buffer, 512, 1, img_file);
-            }
-            if (!openfile)
-            {
-
-            }
-            return 0;
-        }
 
     }
-    fclose(inptr);
-    fclose(img_file);
-    return 0;
+
+
+
+    //read the raw file
+
+    BYTE buffer[512];
+
+    int jpegcount = 0;
+
+    int searchjpeg = 1; //1 if true, 0 if false
+
+
+
+
+
+    // pointer to outfile
+
+    FILE* outptr = NULL;
+
+
+
+    // go through the cardfile until there aren't any blocks left
+
+    while (fread(buffer, 512, 1, inptr) == true)
+
+        {
+
+            if (searchjpeg == 1)
+
+                {
+
+                    //find the beginning of JPEG
+
+                    if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+
+                    {
+
+                        char jpegname[8];
+
+                        searchjpeg = 0; //yes, found
+
+                        sprintf(jpegname, "%03d.jpg", jpegcount);
+
+                        outptr = fopen(jpegname, "w");
+
+                        fwrite(&buffer, 512, 1, outptr);
+
+                    }
+
+                    else
+
+                    {
+
+                        printf("nothing\n"); //not found
+
+                    }
+
+                }
+
+
+
+                else
+
+                {
+
+                    //already found a jpeg?
+
+                    if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] == 0xe0 || buffer[3] == 0xe1))
+
+                    {
+
+                        //yes
+
+                        char jpegname[8];
+
+                        fclose(outptr);
+
+                        jpegcount++;
+
+                        sprintf(jpegname, "%03d.jpg", jpegcount);
+
+                        outptr = fopen(jpegname, "w");
+
+                        fwrite(&buffer, 512, 1, outptr);
+
+                    }
+
+                    else
+
+                    { //no
+
+                    fwrite(&buffer, 512, 1, outptr);
+
+                    }
+
+                }
+
+        }
+
+
+
+        // close files
+
+        fclose(inptr);
+
+        fclose(outptr);
+
+        free(buffer);
+
+        return 0;
 
 }
