c3b9580c27a081495dcfd7ac14ec63289315a844
norahev <norahev@users.noreply.github.com>
2020-03-26 11:23:14 +0000

    automated commit by submit50

--- roster.py
+++ roster.py
@@ -4,12 +4,9 @@ from sys import exit, argv
 if len(argv) != 2:  # exit if incorrect usage
     print("Incorrect number of command-line arguments")
     exit(1)
-#if argv[1] != 'Gryffindor' or argv[1] != 'Hufflepuff' or argv[1] != 'Slytherin' or argv[1] != 'Ravenclaw':
-   # print("Please enter correct house name")
-   # exit(1)
 db = cs50.SQL("sqlite:///students.db")  # open students database
-liststudents = db.execute(f"SELECT * FROM students WHERE house = '{argv[1]}' ORDER BY last, first")  # list students from corresponding houses
-
+# list students from corresponding houses
+liststudents = db.execute(f"SELECT * FROM students WHERE house = '{argv[1]}' ORDER BY last, first")
 middle = ""
 for i in liststudents:
     if i['middle'] != None:
@@ -18,4 +15,3 @@ for i in liststudents:
     else:
         middle = ""
         print(f"{i['first']} {i['last']}, born in {i['birth']}")
-
