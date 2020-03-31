0e0fbb39b27d88b26b2ee59aab1cb05b4295f5b4
norahev <norahev@users.noreply.github.com>
2020-03-31 12:02:14 +0000

    automated commit by submit50

---
+++ Pokedex/app/src/main/java/cs50/pokedex/Pokemon.java
@@ -0,0 +1,17 @@
+package cs50.pokedex;
+
+public class Pokemon {
+    private String name;
+    private String url;
+
+    Pokemon(String name, String url){
+        this.name = name;
+        this.url = url;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getURl() {
+        return url;
+    }
+}
